7726affad4acfa886446195ed84241447ba98697
sawyer tarr <sawyertarr@outlook.com>
2023-02-24 15:15:28 -0600

    Week3

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,47 +10,156 @@ namespace ChallengesWithTestsMark8
     {
         public bool ArrayContainsAFalse(bool[] vals)
         {
-            throw new NotImplementedException();
+            foreach (bool i in vals) 
+            {
+                if (i == false)
+                {
+                    return true;
+                }
+               
+            }
+            return false;
         }
 
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
-            throw new NotImplementedException();
+            if ( numbers == null || numbers.Count() == 0)
+            {
+                return false;
+            }
+            int sum = 0;
+            foreach (var item in numbers)
+            {
+                if (item % 2 != 0)
+                {
+                    sum += item;
+                }
+            }
+            if (sum % 2 != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            //var sum = numbers.Where(x => x % 2 != 0).Sum(x => x);
+
+            //if (numbers == null)
+            //{
+            //    return false;
+            //}
+            //else if (sum % 2 != 0)
+            //{
+            //    return true;
+            //}
+            //else
+            //{
+            //    return false;
+            //}
+
         }
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
         {
-            throw new NotImplementedException();
+            bool isUpper = false;
+            bool isLower = false;
+            bool isNumber = false;
+            foreach (var ch in password)
+            {
+                if (char.IsLower(ch))
+                {
+                    isLower = true;
+                }
+                if (char.IsUpper(ch))
+                {
+                    isUpper = true;
+
+                }
+                if (char.IsNumber(ch))
+                {
+                    isNumber = true;
+                }
+
+            }
+            if (isLower && isUpper && isNumber)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public char GetFirstLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            char[] firstLetter = val.ToCharArray();
+
+            return firstLetter[0];
         }
 
         public char GetLastLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            char[] lastLetter = val.ToCharArray();
+
+            return lastLetter[lastLetter.Length - 1];
         }
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
-            throw new NotImplementedException();
+            if (dividend == 0 || divisor == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return dividend / divisor;
+            }
+            
         }
 
         public int LastMinusFirst(int[] nums)
         {
-            throw new NotImplementedException();
+            return nums[nums.Length - 1] - nums[0];
         }
 
         public int[] GetOddsBelow100()
         {
-            throw new NotImplementedException();
+            var list = new List<int>();
+            for (int i = 1; i < 100; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    list.Add(i);
+                }
+            }
+            return list.ToArray();
+
+            //int[] below100 = new int[50];
+
+            //var belowList = below100.ToList();
+
+            //for (int i = 1; i < 100; i++)
+            //{
+            //    if (i % 2 != 0)
+            //    {
+            //        belowList.Add(i);
+            //    }
+
+            //}
+            //below100 = belowList.ToArray();
+
+            //return below100;
+
         }
 
         public void ChangeAllElementsToUppercase(string[] words)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = words[i].ToUpper();
+            }
         }
     }
 }
